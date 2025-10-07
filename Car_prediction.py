bb4dc5fd3a8c9fde1a5724c23bc3d784b0cffe55
Nirucoder <152915305+Nirucoder@users.noreply.github.com>
2025-10-07 18:22:41 +0530

    Add files via upload

---
+++ Car_prediction.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn import metrics
+car_dataset = pd.read_csv(r"C:\\Users\\user\\OneDrive\Desktop\vs coding\\ai ml\\archive\\car data.csv")
+car_dataset.head()
+car_dataset.shape
+car_dataset.isnull().sum
+car_dataset.replace({'Fuel_Type':{'Petrol':0,'Diesel':1,'CNG':2}},inplace=True)
+car_dataset.replace({'Seller_Type':{'Dealer':0,'Individual':1}},inplace=True)
+car_dataset.replace({'Transmission':{'Manual':0,'Automatic':1}},inplace=True)
+X= car_dataset.drop(['Car_Name','Selling_Price'],axis=1)
+Y= car_dataset['Selling_Price']
+print(X)
+print(Y)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.1,random_state=2)
+lin_reg_model=LinearRegression()
+lin_reg_model.fit(X_train,Y_train) 
+traning_data_prediction=lin_reg_model.predict(X_train)
+error_score=metrics.r2_score(Y_train,traning_data_prediction)
+print("R SQUARED ERROR FOR LINEAR REGRESSON(TRAIN DATA):",error_score)
+plt.scatter(Y_train,traning_data_prediction)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual Prices vs Predicted Prices")
+plt.show
+test_data_prediction=lin_reg_model.predict(X_test)
+error_score=metrics.r2_score(Y_test,test_data_prediction)
+print("R SQUARED ERROR FOR LINEAR REGRESSION(TEST DATA):",error_score)
+plt.scatter(Y_test,test_data_prediction)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual Prices vs Predicted Prices")
+plt.show
+lass_reg_model=Lasso()
+lass_reg_model.fit(X_train,Y_train) 
+traning_data_prediction=lass_reg_model.predict(X_train)
+error_score=metrics.r2_score(Y_train,traning_data_prediction)
+print("R SQUARED ERROR FOR LASSO(TRAINING DATA):",error_score)
+plt.scatter(Y_train,traning_data_prediction)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual Prices vs Predicted Prices")
+plt.show
+test_data_prediction=lass_reg_model.predict(X_test)
+error_score=metrics.r2_score(Y_test,test_data_prediction)
+print("R SQUARED ERROR FOR LASSO(TEST DATA):",error_score)
+plt.scatter(Y_test,test_data_prediction)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual Prices vs Predicted Prices")
+plt.show
